39d3b43c564c44a92e74668367689580a2c18f33
Óscar Fernández Rodríguez <e.oscar12@go.ugr.es>
2025-10-30 20:10:55 +0100

    Subiendo la practica 2.3

--- routes/router_tienda.js
+++ routes/router_tienda.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res)=>{
     const busqueda =req.query.busqueda;
     if(busqueda){
       const productosEncontrados = await Producto.find({texto_1: {$regex: busqueda, $options: 'i'}})
-      res.render('portada.html', {productos_random: productosEncontrados} )
+      res.render('portada.html', {productos : productosEncontrados} )
     }
     else{
       const productos = await Producto.find({})   // todos los productos
@@ -17,7 +17,7 @@ router.get('/', async (req, res)=>{
       let productos_random=[] //Array de productos random 
       let indices=[]//Array de indices
       //Agregamos tres índices 
-      for(let i=0; i<12; i++){
+      for(let i=0; i<15; i++){
         let control=true
         while(control==true){
           let indice=Math.floor(Math.random()*productos.length)
@@ -31,7 +31,7 @@ router.get('/', async (req, res)=>{
       //Agregamos los tres productos.
       for(let i=0; i<indices.length; i++)
         productos_random.push(productos[indices[i]])
-      res.render('portada.html', { productos, productos_random })    // ../views/portada.html, 
+      res.render('portada.html', { productos : productos_random })    // ../views/portada.html, 
     }
   } catch (err) {                                // se le pasa { productos:productos }
 	console.error(err)
@@ -40,5 +40,45 @@ router.get('/', async (req, res)=>{
 })
 
 // ... más rutas aquí en la siguiente sesión
+router.get('/al_carrito/', async (req, res)=>{
+  const producto = req.query.productoId;
+  req.session.carrito.push(producto);
+  let productosCarrito = [];
+  let precioTotal = 0;
+
+  for (let i = 0; i < req.session.carrito.length; i++)
+    productosCarrito.push(await Producto.findById({ _id: req.session.carrito[i] }));
+  
+
+  for (let i = 0; i < productosCarrito.length; i++)
+    if (productosCarrito[i].precio_rebajado > 0)
+      precioTotal += productosCarrito[i].precio_rebajado
+    else 
+      precioTotal +=productosCarrito[i].precio_euros
+  precioTotal=precioTotal.toFixed(2)   
+  const productos = await Producto.find({}); // todos los productos
+  // elegir 3 aquí
+  let productos_random = []; //Array de productos random
+  let indices = []; //Array de indices
+  //Agregamos tres índices
+  for (let i = 0; i < 15; i++) {
+    let control = true;
+    while (control == true) {
+      let indice = Math.floor(Math.random() * productos.length);
+      if (indices.includes(indice) == false) {
+        indices.push(indice);
+        control = false;
+      }
+    }
+  }
+  //Agregamos los tres productos.
+  for(let i=0; i<indices.length; i++)
+    productos_random.push(productos[indices[i]])
+  res.render("portada.html", {
+    productos: productos_random,
+    totalProductos: productosCarrito.length,
+    precioTotal,
+  });
+})
 
 export default router
--- tienda.js
+++ tienda.js
@@ -1,6 +1,7 @@
 // tienda.js 
 import express   from "express"
 import nunjucks  from "nunjucks"
+import session from "express-session"
 import TiendaRouter from "./routes/router_tienda.js"      
 import connectDB from "./model/connectDB.js"
 await connectDB()
@@ -18,6 +19,24 @@ nunjucks.configure('views', {         // directorio 'views' para las plantillas 
 app.set('view engine', 'html')
 
 app.use('/static', express.static('public'))     // directorio public para archivos css, js, imágenes, etc.
+
+app.use(session({
+	secret: 'my-secret',      // a secret string used to sign the session ID cookie
+	resave: false,            // don't save session if unmodified
+	saveUninitialized: false  // don't create session until something stored
+}))
+
+
+app.use((req, res, next) => {
+	if (!req.session.carrito) {
+		req.session.carrito=[]
+	}
+
+	next()
+});
+
+
+
 app.use("/", TiendaRouter);
 
 // test para el servidor
@@ -30,7 +49,10 @@ app.get("/test", (req, res) => {
   res.render('test.html');
 });
 
+
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutandose en  http://localhost:${PORT}`);
 })
+
