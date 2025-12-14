942c1738ac36abb76d2c475374853df12e2bbe61
Óscar Fernández Rodríguez <e.oscar12@go.ugr.es>
2025-12-14 17:45:32 +0100

    Añadiendom la práctica 5.1 mejorada

--- public/cambio-precio.js
+++ public/cambio-precio.js
@@ -1,48 +1,42 @@
 //cambio-precio.js
 const cambiar_precio = (evt) => {
-    const botón = evt.target;
+    const boton = evt.target;
 
     //identificar a que producto pertenece
-    const id = botón.dataset.productoId; // Obtiene el ID del atributo data-producto-id
+   const id = boton.closest('.mt-auto').querySelector('.productoID').value; //obtenemos el id del producto
 
     //identificar el <input> que le corresponde
-    const input = botón.parentElement.querySelector('.input-precio');
+    const inputPrecio = boton.closest('.cambiar_precio').querySelector('.input-precio'); //Busca el input del elemento padre 
+    
 
     //tomar el valor de entrada del input
-    const entrada_del_input = input.value;
+    const nuevoPrecio=inputPrecio.value; //toma el valor del input
 
     fetch(`/api/productos/${id}`, { // Ajusta la URL según tu API
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json;charset=UTF-8'
         },
-        body: JSON.stringify({precio_euros: entrada_del_input})
+        body: JSON.stringify({precio_euros: nuevoPrecio})
     })
     .then(res => res.json())
     .then(res => {
         console.log(res)
-        //poner el precio actualizado
-        const contenedor = botón.closest('.mt-auto');
-        const textoPrecio = contenedor.querySelector('p.card-text'); // Elemento donde se muestra el precio
-        if (textoPrecio) {
-            const delTag = textoPrecio.querySelector('del');
-            if (delTag) {
-                delTag.textContent = `${entrada_del_input}€`;
-            } else {
-                textoPrecio.textContent = `${entrada_del_input}€`;
-            }
-        }
-        alert("Precio actualizado correctamente");
+        //poner precio actualizado
+        const parrafoPrecio=boton.closest('.mt-auto').querySelector('.precio_actualizar'); //buscamos el texcto del precio en elelemento padre
+        parrafoPrecio.textContent=`${nuevoPrecio}€`;  //Actualizamos el precio en el párrafo
+        
     })
     .catch(err=> {
         console.error(err)
         //poner mensaje de error
-        alert("Error al actualizar el precio");
+        alert('Error al actualizar el precio');
+
     })
 }
 
 //Sacar los botones _cambiar_precio aquí
-const botones_cambiar_precio = document.querySelectorAll('.boton_cambiar_precio'); // Selecciona todos los botones
+const botones_cambiar_precio = document.querySelectorAll('.boton_cambiar_precio');
 
 for(const botón of botones_cambiar_precio){
     botón.addEventListener('click', cambiar_precio)
